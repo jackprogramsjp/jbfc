a74bf2cae0321e96c7c1fc44d8725a0abc32c473
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2022-04-23 20:29:03 -0700

    Update production.sh

--- production.sh
+++ production.sh
@@ -6,13 +6,19 @@
 #     deno compile --output "jbfc-$1" --target $1 _build/default/main.bc.js
 # }
 
-if type "deno" > /dev/null; then
-    chmod +x ./build.sh
-    ./build.sh
-    yes | cp -rf _build/default/main.bc.js jbfc.js
-    (echo '#!/usr/bin/env node' && cat jbfc.js) > jbfc1.js && yes | mv jbfc1.js jbfc.js
-    rm -rf jbfc1.js
-else
-    echo "Error: deno is not installed in this system" >&2
-    exit 1
-fi
+# if type "deno" > /dev/null; then
+#     chmod +x ./build.sh
+#     ./build.sh
+#     yes | cp -rf _build/default/main.bc.js jbfc.js
+#     (echo '#!/usr/bin/env node' && cat jbfc.js) > jbfc1.js && yes | mv jbfc1.js jbfc.js
+#     rm -rf jbfc1.js
+# else
+#     echo "Error: deno is not installed in this system" >&2
+#     exit 1
+# fi
+
+chmod +x ./build.sh
+./build.sh
+yes | cp -rf _build/default/main.bc.js jbfc.js
+(echo '#!/usr/bin/env node' && cat jbfc.js) > jbfc1.js && yes | mv jbfc1.js jbfc.js
+rm -rf jbfc1.js
